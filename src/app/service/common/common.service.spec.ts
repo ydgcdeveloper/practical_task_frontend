022b19e48fd142a3732648d1c7384630144c5e94
Yan David <yan.gomez@qualud.es>
2022-12-08 13:55:53 -0500

    add page services

---
+++ src/app/service/common/common.service.spec.ts
@@ -0,0 +1,16 @@
+import { CommonService } from './common.service';
+import { TestBed } from '@angular/core/testing';
+
+
+describe('CommonService', () => {
+  let service: CommonService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(CommonService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
