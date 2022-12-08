217b32e38142240f7a4e1e00b02d4b388cd69afb
Yan David <yan.gomez@qualud.es>
2022-12-08 13:51:39 -0500

    add  gateway component

---
+++ src/app/component/gateway/gateway.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GatewayComponent } from './gateway.component';
+
+describe('GatewayComponent', () => {
+  let component: GatewayComponent;
+  let fixture: ComponentFixture<GatewayComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ GatewayComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(GatewayComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
