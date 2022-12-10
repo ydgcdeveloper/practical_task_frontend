df34ca74be56550a337dd11e96e1e1968fc3d138
Yan David <yan.gomez@qualud.es>
2022-12-09 22:42:15 -0500

    add new routes to routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./index/index.module').then(m => m.IndexModule) },
   { path: 'device', loadChildren: () => import('./device/device.module').then(m => m.DeviceModule) },
-  { path: 'gateway', loadChildren: () => import('./gateway/gateway.module').then(m => m.GatewayModule) }];
+  { path: 'gateway', loadChildren: () => import('./gateway/gateway.module').then(m => m.GatewayModule) },
+  { path: 'edit-gateway', loadChildren: () => import('./edit-gateway/edit-gateway.module').then(m => m.EditGatewayModule) },
+  { path: 'add-gateway', loadChildren: () => import('./add-gateway/add-gateway.module').then(m => m.AddGatewayModule) },
+  { path: '**', component: PageNotFoundComponent }
+];
+  
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
